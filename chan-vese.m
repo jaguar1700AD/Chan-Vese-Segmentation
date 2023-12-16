2f2895f3ffb94f3bf2bbbf99b0de33628eabbac8
jaguar1700AD <37709963+jaguar1700AD@users.noreply.github.com>
2023-12-15 18:49:01 -0800

    First version of Code complete

--- chan-vese.m
+++ chan-vese.m
@@ -1,9 +1,13 @@
 %% Read an image from a file
-f = imread('testpat_blur4.png');
+f = imread('ellipse_blur2.png');
 
 % Set constants
 neta = 10^(-8);
-mu = 0.1;
+mu = 0.2;
+nu = 0;
+lambda1 = 1; lamnda2 = 1;
+dt = 0.5;
+tol = 10^(-3);
 global epsilon = 1;
 
 % convert image to double and scale to [0,1]
@@ -16,7 +20,7 @@ function display_image(f)
   imagesc(f);
   caxis([0 1])
   colormap(gray)
-  axis equal, axis tight
+  %axis equal, axis tight
 endfunction
 
 function result = dirac_delta(A)
@@ -27,6 +31,14 @@ endfunction
 function result = heaviside(A)
   global epsilon;
   result = (1 + (2 * atan(A / epsilon) / pi)) / 2;
+endfunction
+
+function result = integrate(A)
+  result = sum(sum(A));
+endfunction
+
+function result = average(f, density)
+  result = integrate(f .* density) / integrate(density);
 endfunction
 
 %Extend f to outside the image boundary using 0 Neumann Boundary Condition
@@ -69,24 +81,52 @@ endfunction
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Initialize phi
-[x, y] = meshgrid(1:n2, 1:n1);
-phi = sin(pi * x / 5) * sin(pi * y / 5);
-phi = extend(phi);
+x = 1:n2; y = (1:n1)';
+[xx, yy] = meshgrid(x, y);
+phi = sin(pi * x / 5) .* sin(pi * y / 5);
 
-for step = 1:100
+step = 1;
+while (1)
 
-  phi_i_plus = row_forward_diff(phi);
-  phi_i_minus = row_backward_diff(phi);
-  phi_i_zero = row_central_diff(phi);
+  phi_extended = extend(phi); % Extend using 0 neumann bdd condns
 
-  phi_j_plus = col_forward_diff(phi);
-  phi_j_minus = col_backward_diff(phi);
-  phi_j_zero = col_central_diff(phi);
+  phi_i_plus = row_forward_diff(phi_extended);
+  phi_i_minus = row_backward_diff(phi_extended);
+  phi_i_zero = row_central_diff(phi_extended);
 
-  A = mu ./ (neta^2 + phi_i_plus.^2 + phi_j_zero.^2)^(0.5);
-  B = mu ./ (neta^2 + phi_i_zero.^2 + phi_j_plus.^2)^(0.5);
+  phi_j_plus = col_forward_diff(phi_extended);
+  phi_j_minus = col_backward_diff(phi_extended);
+  phi_j_zero = col_central_diff(phi_extended);
 
-endfor
+  A = mu ./ ((neta^2 + phi_i_plus.^2 + phi_j_zero.^2).^(0.5));
+  B = mu ./ ((neta^2 + phi_i_zero.^2 + phi_j_plus.^2).^(0.5));
+
+  c1 = average(f, heaviside(phi));
+  c2 = average(f, 1 - heaviside(phi));
+
+  % Do all difference computations using phi_extended to enforce 0 Neumann bdd condns
+  Dphi = restrict(row_backward_diff(A .* phi_i_plus) + col_backward_diff(B .* phi_j_plus));
+  Dphi += -nu - lambda1 * ((f - c1).^2) + lamnda2 * ((f - c2).^2);
+  Dphi = Dphi .* dirac_delta(phi);
+
+  phi_new = phi + Dphi * dt;
+
+  if (rem(step, 100) == 0)
+    step
+    display_image(f);
+    hold on;
+    contour(x, y, phi_new, [0,0], 'r');
+    drawnow
+    pause(0.001);
+  endif
+
+  if (norm(reshape(phi_new - phi, n1*n2, 1), 2) / sqrt(n1*n2) < tol)
+    break;
+  endif
+  phi = phi_new;
+  step += 1;
+
+endwhile
 
 
 %% Output original and result side-by-side
