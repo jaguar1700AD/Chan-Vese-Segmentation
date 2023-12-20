2a939bd70053400cdba80c502a0e6c35a8a3321d
jaguar1700AD <37709963+jaguar1700AD@users.noreply.github.com>
2023-12-19 23:53:18 -0800

    Extended code for RGB images

--- chan-vese.m
+++ chan-vese.m
@@ -1,4 +1,4 @@
-file = "dog.png";
+file = "rgb_apple.jpg";
 
 % Set constants
 neta = 10^(-8);
@@ -13,16 +13,16 @@ global epsilon = 1;
 f = imread(file);
 info = imfinfo(file);
 
-if (length(size(f)) ~= 2)
-  error("Not a grayscale image");
+if (length(size(f)) ~= 3 || size(f)(3) ~= 3)
+  error("Not an RGB image");
 end
 
 % convert image to double and scale to [0,1]
 f = double(f);
 f = f / 2^(info.BitDepth);
 
-[n1,n2] = size(f)
-LargestPixel = norm(reshape(f, n1*n2, 1), inf)
+[n1,n2, n3] = size(f)
+LargestPixel = norm(reshape(f, n1*n2*n3, 1), inf)
 
 function display_image(f)
   figure(1); clf;
@@ -30,7 +30,7 @@ function display_image(f)
   imagesc(f);
   %imshow(f);
   caxis([0 1])
-  colormap(gray)
+  %colormap(gray)
   %axis equal, axis tight
 endfunction
 
@@ -54,39 +54,39 @@ endfunction
 
 %Extend f to outside the image boundary using 0 Neumann Boundary Condition
 function result = extend(f)
-  A = [f(:,1), f, f(:,end)];
-  result = [A(1,:); A; A(end, :)];
+  A = [f(:,1, :), f, f(:,end, :)];
+  result = [A(1,:, :); A; A(end, :,:)];
 endfunction
 
 function result = restrict(f)
-  result = f(2:end-1, 2:end-1);
+  result = f(2:end-1, 2:end-1,:);
 endfunction
 
 %%% Functions for various differences. Coded in such a way that the output
 %% is 0 whenever a difference doesn't make sense
 
 function result = row_forward_diff(f)
-  result = [f(2:end,:); f(end,:)] - f;
+  result = [f(2:end,:,:); f(end,:,:)] - f;
 endfunction
 
 function result = row_backward_diff(f)
-  result = f - [f(1,:); f(1:end-1,:)];
+  result = f - [f(1,:,:); f(1:end-1,:,:)];
 endfunction
 
 function result = row_central_diff(f)
-  result = ([f(2:end, :); f(end-1,:)] - [f(2,:); f(1:end-1,:)]) / 2;
+  result = ([f(2:end, :,:); f(end-1,:,:)] - [f(2,:,:); f(1:end-1,:,:)]) / 2;
 endfunction
 
 function result = col_forward_diff(f)
-  result = [f(:, 2:end), f(:, end)] - f;
+  result = [f(:, 2:end,:), f(:, end,:)] - f;
 endfunction
 
 function result = col_backward_diff(f)
-  result = f - [f(:, 1), f(:, 1:end-1)];
+  result = f - [f(:, 1,:), f(:, 1:end-1,:)];
 endfunction
 
 function result = col_central_diff(f)
-  result = ([f(:, 2:end), f(:, end-1)] - [f(:, 2), f(:, 1:end-1)]) / 2;
+  result = ([f(:, 2:end,:), f(:, end-1,:)] - [f(:, 2,:), f(:, 1:end-1,:)]) / 2;
 endfunction
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -114,12 +114,13 @@ while (1)
   A = mu ./ ((neta^2 + phi_i_plus.^2 + phi_j_zero.^2).^(0.5));
   B = mu ./ ((neta^2 + phi_i_zero.^2 + phi_j_plus.^2).^(0.5));
 
-  c1 = average(f, heaviside(phi));
-  c2 = average(f, 1 - heaviside(phi));
+  d = heaviside(phi);
+  c1 = average(f, d);
+  c2 = average(f, 1 - d);
 
   % Do all difference computations using phi_extended to enforce 0 Neumann bdd condns
   Dphi = restrict(row_backward_diff(A .* phi_i_plus) + col_backward_diff(B .* phi_j_plus));
-  Dphi += -nu - lambda1 * ((f - c1).^2) + lamnda2 * ((f - c2).^2);
+  Dphi = Dphi - nu - lambda1 * sum((f - c1).^2,3) + lamnda2 * sum((f - c2).^2, 3);
   Dphi = Dphi .* dirac_delta(phi);
 
   phi_new = phi + Dphi * dt;
